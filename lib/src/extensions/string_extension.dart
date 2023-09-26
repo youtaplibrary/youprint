e42b2b70f9ed2fcf6216559c4fca85705bb3115f
Robby Surya Pratama <robbysoerya6@gmail.com>
2023-09-26 16:22:58 +0700

    refactor: split by length function

--- lib/src/extensions/string_extension.dart
+++ lib/src/extensions/string_extension.dart
@@ -1,11 +1,35 @@
 extension StringExtension on String {
   List<String> splitByLength(int length) {
     List<String> pieces = [];
-
-    for (int i = 0; i < this.length; i += length) {
-      int offset = i + length;
-      pieces.add(substring(i, offset >= this.length ? this.length : offset));
+    if (!contains(' ')) {
+      for (int i = 0; i < this.length; i += length) {
+        int offset = i + length;
+        pieces.add(substring(i, offset >= this.length ? this.length : offset));
+      }
+      return pieces;
     }
+
+    List<String> words = split(' ');
+    String currentLine = '';
+    for (int i = 0; i < words.length; i++) {
+      String word = words[i];
+      if (currentLine.isEmpty) {
+        currentLine = word;
+      } else {
+        String testLine = '$currentLine $word';
+        if (testLine.length <= length) {
+          currentLine = testLine;
+        } else {
+          pieces.add(currentLine);
+          currentLine = word;
+        }
+      }
+    }
+
+    if (currentLine.isNotEmpty) {
+      pieces.add(currentLine);
+    }
+
     return pieces;
   }
 }
