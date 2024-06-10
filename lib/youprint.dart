eb2cde4b23f0553f73fdf6537af0b0f53f05c800
Robby Surya Pratama <robby.pratama@youtap.id>
2024-06-11 00:21:59 +0700

    refactor: rebase to master and adjustment

--- lib/youprint.dart
+++ lib/youprint.dart
@@ -1,7 +1,5 @@
 library youprint;
 
-export 'package:flutter_blue_plus/flutter_blue_plus.dart';
-
 export 'src/async/async_esc_pos_printer.dart';
 export 'src/connection/connection.dart';
 export 'src/esc_pos_charset_encoding.dart';
