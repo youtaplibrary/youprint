7c2f5ed403be965ba9123ac179144520117935a9
Robby Surya Pratama <robbysoerya6@gmail.com>
2025-03-07 11:35:53 +0700

    feat: configuration printer 80mm base on 1.0.3

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -6,9 +6,23 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<fluetooth/FluetoothPlugin.h>)
+#import <fluetooth/FluetoothPlugin.h>
+#else
+@import fluetooth;
+#endif
+
+#if __has_include(<permission_handler_apple/PermissionHandlerPlugin.h>)
+#import <permission_handler_apple/PermissionHandlerPlugin.h>
+#else
+@import permission_handler_apple;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FluetoothPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluetoothPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
 }
 
 @end
